1d5002385f94a4f54fb8b8053d6f14c5996705c4
nathanljustin <njustin@g.hmc.edu>
2017-05-21 13:42:50 -0700

    Unzipped file

---
+++ prework2/prework2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  prework2
+//
+//  Created by Nathan Justin on 5/20/17.
+//  Copyright © 2017 Nathan Justin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
